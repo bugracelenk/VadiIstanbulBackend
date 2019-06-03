14e63f0c1f40783b63fca9cf20c20f9c75cdedc1
Altuğ Buğra Çelenk <bugracelenk01@gmail.com>
2019-06-03 22:39:44 +0300

    created services model

---
+++ models/Service.js
@@ -0,0 +1,22 @@
+const sql = require("./db");
+
+exports.getService = (id, result) => {
+  sql.query("SELECT * FROM services WHERE id = ?", [id], (err, res) => {
+    if (err) result(err, null);
+    else result(null, res);
+  });
+};
+
+exports.getServices = result => {
+  sql.query("SELECT * FROM services", (err, res) => {
+    if (err) result(err, null);
+    else result(null, res);
+  });
+};
+
+exports.createService = (serviceData, result) => {
+  sql.query("INSERT INTO services SET ?", [serviceData], (err, res) => {
+    if (err) result(err, null);
+    else result(null, res);
+  });
+};
---
+++ routes/services.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const servicesControllers = require("../controllers/servicesControllers");
+const checkAuth = require("../middlewares/check_auth");
+const checkSaler = require("../middlewares/check_saler");
+
+router
+  .get("/", servicesControllers.getServices)
+  .get("/:serviceId", servicesControllers.getService)
+  .post("/", checkAuth, checkSaler,servicesControllers.createService)
+
+module.exports = router;
